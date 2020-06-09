cc67f420fbd233c14389b641a4497ebf1e86ac6e
Michael Incardona <michael.incardona@outlook.com>
2020-06-08 20:02:58 -0400

    Initial project skeleton

---
+++ src/ipsapply.c
@@ -0,0 +1,10 @@
+#include "config_ipsapply.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+    (void)argc;
+    (void)argv;
+    printf("ips patcher\n");
+    return 0;
+}
